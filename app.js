79075cec97f60f83b74c032ad803e0e7beeda1d0
Misael Calvillo Mancilla <misa@homely.mx>
2022-11-10 20:26:25 -0600

    Agrega saludo para el jugador

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 const nombre = prompt('Cómo te llamas? ');
 
+console.log(`Hola ${nombre}, bienvenido al juego...`);
+
 let keepRunning = true;
 while(keepRunning) {
   const res = prompt(`Cuál es tu respuesta ${nombre}? `);
